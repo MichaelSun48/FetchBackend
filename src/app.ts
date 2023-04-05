051bd858aba3850c770b3ae22cda30cab6f7673c
Michael Sun <ms48@illinois.edu>
2023-04-04 20:39:17 -0500

    Implemented both endpoints according to spec. Added gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import { Receipt } from './Types/Receipt.js';
+import { Receipt } from './types/Receipt.js';
+import { calcPoints } from './utils/utils.js';
 import { v4 as uuid } from 'uuid';
 import { is } from 'typia';
 
@@ -14,14 +15,26 @@ let map = new Map<string, Receipt>();
 
 
 app.post('/receipts/process', (req, res) => {
-    let receipt = req.body
+    let receipt: Receipt = req.body
     if (!is<Receipt>(receipt)) { 
         // Incorrect type 
+        res.status(400).send("The receipt is invalid")
     } 
+    let id: string = uuid();
+    map.set(id, receipt)
+    res.status(200).send(id)
 })
 
 app.get('/receipts/:id/points', (req, res) => {
     let id: string = req.params.id
+
+    let receipt = map.get(id); 
+    if (receipt == undefined) {
+        res.status(400).send("No receipt found for that id")
+    } 
+    res.status(200).send({
+        "points": calcPoints(receipt!)
+    })
 })
 
 app.listen(port, () => {
